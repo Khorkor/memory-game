2508a97fae57d9dfc4437dc1a20089abc7845567
Khorkor <32433400+Khorkor@users.noreply.github.com>
2017-12-12 18:18:27 +0300

    Add files via upload

---
+++ project-memory game/README.md
@@ -0,0 +1,9 @@
+# Memory Game Project
+
+This game was built as a Udacity Course project. The purpose of the project is to demonstrate mastery of HTML, CSS, and JavaScript.
+
+# How to Play
+
+Just click the box to flip the card, if two fliped card are the same in look, it will be fliped. else it will still in back.
+
+You will get start according to your good memory.
---
+++ project-memory game/js/game.js
@@ -0,0 +1,212 @@
+// Shuffle function 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+};
+
+
+var deck = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor",
+           "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf",
+           "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
+
+// Game state variables
+var open = [];
+var matched = 0;
+var mcounter = 0;
+var stars = 3;
+var timer = {
+    seconds: 0,
+    minutes: 0,
+    clearTime: -1
+};
+
+// Difficulty settings
+var hard = 20;
+var medium = 25;
+
+var modal = $("#win-modal");
+
+
+// Interval function to be called every second, increments timer and updates HTML
+var startTimer = function() {
+    if (timer.seconds === 59) {
+        timer.minutes++;
+        timer.seconds = 0;
+    } else {
+        timer.seconds++;
+    }
+
+    var formattedSec = "0";
+    if (timer.seconds < 10) {
+        formattedSec += timer.seconds
+    } else {
+        formattedSec = String(timer.seconds);
+    }
+
+    var time = String(timer.minutes) + ":" + formattedSec;
+    $(".timer").text(time);
+};
+
+// Resets timer state and restarts timer
+function resetTimer() {
+    clearInterval(timer.clearTime);
+    timer.seconds = 0;
+    timer.minutes = 0;
+    $(".timer").text("0:00");
+
+    timer.clearTime = setInterval(startTimer, 1000);
+};
+
+// Randomizes cards on board and updates card HTML
+function updateCards() {
+    deck = shuffle(deck);
+    var index = 0;
+    $.each($(".card i"), function(){
+      $(this).attr("class", "fa " + deck[index]);
+      index++;
+    });
+    resetTimer();
+};
+
+// Toggles win modal on
+function showModal() {
+    modal.css("display", "block");
+};
+
+// Removes last start from remaining stars, updates modal HTML
+function removeStar() {
+    $(".fa-star").last().attr("class", "fa fa-star-o");
+    stars--;
+    $(".num-stars").text(String(stars));
+};
+
+// Restores star icons to 3 stars, updates modal HTML
+function resetStars() {
+    $(".fa-star-o").attr("class", "fa fa-star");
+    stars = 3;
+    $(".num-stars").text(String(stars));
+};
+
+// Updates number of moves in the HTML, removes star is necessary based on difficulty variables
+function updatemcounter() {
+    $(".moves").text(mcounter);
+
+    if (mcounter === hard || mcounter === medium) {
+        removeStar();
+    }
+};
+
+// Checks if card is a valid move (if it not currently matched or open)
+function isValid(card) {
+    return !(card.hasClass("open") || card.hasClass("match"));
+};
+
+// check if currently open cards match
+function checkMatch() {
+    if (open[0].children().attr("class")===open[1].children().attr("class")) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+// win condition
+function hasWon() {
+    if (matched === 16) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+// Sets currently open cards to the match state, checks win condition
+var setMatch = function() {
+    open.forEach(function(card) {
+        card.addClass("match");
+    });
+    open = [];
+    matched += 2;
+
+    if (hasWon()) {
+        clearInterval(timer.clearTime);
+        showModal();
+    }
+};
+
+// Sets currently open cards back to default state
+var resetOpen = function() {
+    open.forEach(function(card) {
+        card.toggleClass("open");
+        card.toggleClass("show");
+    });
+    open = [];
+};
+
+// Sets selected card to the open and shown state
+function openCard(card) {
+    if (!card.hasClass("open")) {
+        card.addClass("open");
+        card.addClass("show");
+        open.push(card);
+    }
+};
+
+
+
+// Resets all game state variables and resets all required HTML to default state
+var resetGame = function() {
+    open = [];
+    matched = 0;
+    mcounter = 0;
+    resetTimer();
+    updatemcounter();
+    $(".card").attr("class", "card");
+    updateCards();
+    resetStars();
+};
+
+// Handles primary game logic of game
+var onClick = function() {
+    if (isValid( $(this) )) {
+
+        if (open.length === 0) {
+            openCard( $(this) );
+
+        } else if (open.length === 1) {
+            openCard( $(this) );
+            mcounter++;
+            updatemcounter();
+
+            if (checkMatch()) {
+                setTimeout(setMatch, 300);
+
+            } else {
+                setTimeout(resetOpen, 700);
+
+            }
+        }
+    }
+};
+
+// Resets game state and toggles win modal display off
+var playAgain = function() {
+    resetGame();
+    modal.css("display", "none");
+};
+
+
+$(".card").click(onClick);
+$(".restart").click(resetGame);
+$(".play-again").click(playAgain);
+
+// Provides a randomized game board on page load
+$(updateCards);
